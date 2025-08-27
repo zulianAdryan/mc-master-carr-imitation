8531d3832da538aa3d38d234e7c3856b9a300179
zulianAdryan <zulian.adryan@gmail.com>
2025-08-27 17:07:09 +0700

    fix: avoid vercel error logs when helper runs on unready client component

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,4 +1,6 @@
 export const formatterCurrency = (value: number): string => {
+  if (typeof window === "undefined") return value.toString();
+
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -8,6 +10,8 @@ export const formatterCurrency = (value: number): string => {
 };
 
 export const formatterDate = (date: string) => {
+  if (typeof window === "undefined") return date;
+
   const _date = new Date(date);
   return _date.toLocaleString();
 };
